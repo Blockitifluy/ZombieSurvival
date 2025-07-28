f8bab03ed0c4b9db8f08b41b818c3759f6220ce2
Blockitifluy <blockitifluy@gmail.com>
2025-07-28 22:15:55 +0100

    Minor fix to make-build.bat

--- tools/make-build.bat
+++ tools/make-build.bat
@@ -6,14 +6,14 @@ if [%1] equ [] (
 )
 
 REM Building
-dotnet publish "%0\..\ZombieSurvival.csproj" --output %1
+dotnet publish "%0\..\..\ZombieSurvival.csproj" --output %1
 if %errorlevel% neq 0 (
 	echo Build failed
 	exit /b %errorlevel%
 )
 
 REM Coping folders
-xcopy "%0\..\shaders" "%1\shaders" /E /I /Y /Q
-xcopy "%0\..\resources" "%1\resources" /E /I /Y /Q
+xcopy "%0\..\..\shaders" "%1\shaders" /E /I /Y /Q
+xcopy "%0\..\..\resources" "%1\resources" /E /I /Y /Q
 DEL "%1\resources\scenes\*.*" /F /Q
 DEL "%1\resources\meshs\*.*" /F /Q
